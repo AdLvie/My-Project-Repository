32714a5bd887a5faaaaec39f126f64c4b14c0ee7
AdLvie <vladyslavvelikiy777@gmail.com>
2018-09-27 17:02:35 +0300

    project

---
+++ src/Stringlesson/Main.java
@@ -0,0 +1,50 @@
+package Stringlesson;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] arg){
+	
+//		String s1= "Petro";
+//		String s="Petro";
+//		String s2= new String("Petro");
+//		String s4=("");
+//		
+//		System.out.println(s1==s2);
+//		System.out.println(s1==s);
+//		
+//		System.out.println(s1.equals(s2));
+//	
+//		System.out.println(s1.charAt(0));
+//		System.out.println(s1.codePointAt(0));
+//		System.out.println(s1.codePointBefore(1));
+//		System.out.println("o".compareTo("a"));
+//		System.out.println(s1.contains("P"));
+//		System.out.println(s1.endsWith("o"));
+//		System.out.println(s1.equalsIgnoreCase(s));
+//		System.out.println(s1.hashCode());
+//		System.out.println(s.hashCode());
+//		System.out.println(s2.hashCode());
+//		System.out.println(s1.indexOf("e"));
+//		System.out.println(s1.intern() == s2.intern());
+//		System.out.println(s1.isEmpty());
+//		System.out.println(s1.length());
+//		System.out.println(s1.replace("e","a"));
+//		System.out.println(s1.substring(1,3));
+//		char[] charArray = s1.toCharArray();
+//		for (int i=0; i<chararray.length; i++) {
+//		System.out.println(CharArray[i]);	
+//		}
+//	System.out.println(s1.toLowerCase());
+//	System.out.println(s1.toUpperCase());
+//	
+	
+	Scanner sc = new Scanner(System.in);
+	String stringFromConsole= sc.nextLine();
+	System.out.println(stringFromConsole.charAt(4));
+	
+	}
+	
+	
+}
